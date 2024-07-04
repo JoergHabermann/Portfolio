de3a1dbcead58a99754b7b0b8889a60181db5467
Jörg Habermann <joerg1985@gmx.net>
2024-07-04 22:31:07 +0200

    skills icons

--- src/app/main-page/my-skills/my-skills.component.ts
+++ src/app/main-page/my-skills/my-skills.component.ts
@@ -1,12 +1,32 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Skill {
+  name: string;
+  pic: string;
+}
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
+
 export class MySkillsComponent {
 
+  skills: Skill[] = [
+    { name: "Angular", pic: "../../../assets/img/angular.svg" },
+    { name: "TypeScript", pic: "../../../assets/img/typescript.svg" },
+    { name: "JavaScript", pic: "../../../assets/img/javascript.svg" },
+    { name: "HTML", pic: "../../../assets/img/html.svg" },
+    { name: "CSS", pic: "../../../assets/img/css.svg" },
+    { name: "GIT", pic: "../../../assets/img/git.svg" },
+    { name: "Rest-Api", pic: "../../../assets/img/api.svg" },
+    { name: "Firebase", pic: "../../../assets/img/firebase.svg" },
+    { name: "Scrum", pic: "../../../assets/img/scrum.svg" },
+    { name: "Material design", pic: "../../../assets/img/materialdesign.svg" }
+  ];
+  
 }
