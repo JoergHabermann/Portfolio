f1a46dc49e862a5ad8f0465ee587ed983ee70954
Jörg Habermann <joerg1985@gmx.net>
2024-07-06 20:56:30 +0200

    finish portfolio section

--- src/app/main-page/portfolio/portfolio.component.ts
+++ src/app/main-page/portfolio/portfolio.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { ProjectComponent } from './project/project.component';
+import { NgFor } from '@angular/common';
+
+interface Project {
+  title: string;
+  skills: string;
+  description: string;
+  test: string;
+  github: string;
+  image: string;
+  picleft: boolean;
+}
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [ProjectComponent,NgFor],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-
+  projects: Project[] = [
+    {
+      title: 'Join',
+      skills: 'JavaScript | HTML | CSS | Firebase',
+      description: `Task manager inspired by the Kanban System. Create and organize tasks
+            using drag and drop functions, assign users and categories.`,
+      test: '',
+      github: '',
+      image: '../../../assets/img/join.png',
+      picleft: false,
+    },
+    {
+      title: 'Sharkie',
+      skills: 'JavaScript | HTML | CSS | OOP',
+      description: `A simple Jump-and-Run game based on an object-oriented approach. 
+      Help sharkie to find coins and poison bottles to fight against the killer whale.`,
+      test: '',
+      github: '',
+      image: '../../../assets/img/sharkie.png',
+      picleft: true,
+    },
+    {
+      title: 'Pokédex',
+      skills: 'JavaScript | HTML | CSS | API',
+      description: `Based on the PokéAPI a simple library that provides and 
+      catalogues pokemon information.`,
+      test: '',
+      github: '',
+      image: '../../../assets/img/pokedex.png',
+      picleft: false,
+    }    
+  ];
 }
--- src/app/main-page/portfolio/project/project.component.ts
+++ src/app/main-page/portfolio/project/project.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [NgIf,NgClass],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
-
+  @Input() title!: string;
+  @Input() skills!: string;
+  @Input() description!: string;
+  @Input() image!: string;
+  @Input() picleft!: boolean;
 }
