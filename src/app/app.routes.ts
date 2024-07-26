f8222bfff0cfb717518431a3d8b3db1a37edf33a
JoergHabermann <joerg1985@gmx.net>
2024-07-27 01:12:25 +0200

    start impressum

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { LegalComponent } from './legal/legal.component';
 
 export const routes: Routes = [
-    { path: '', component: MainPageComponent},
-    { path: 'main', component: MainPageComponent }   
+    { path: '', component: MainPageComponent },
+    { path: 'main', component: MainPageComponent },   
+    { path: 'legal', component: LegalComponent },
 ];
--- src/app/legal/legal.component.ts
+++ src/app/legal/legal.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../main-page/header/header.component';
+import { FooterComponent } from '../main-page/footer/footer.component';
 
 @Component({
   selector: 'app-legal',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,FooterComponent],
   templateUrl: './legal.component.html',
   styleUrl: './legal.component.scss'
 })
